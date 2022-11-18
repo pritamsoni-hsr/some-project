8b081305c5087310e0a9b7f82052e1ae03ebda8e
Pritam Soni <23050213+pritamsoni-hsr@users.noreply.github.com>
2022-11-19 00:56:25 +0530

    add google and apple client id

--- app/common/vars.ts
+++ app/common/vars.ts
@@ -1,1 +1,4 @@
 export const ApiBaseUrl = 'http://192.168.0.197:8000';
+
+export const iOSClientId = 'in.foodmesh.tnn';
+export const GoogleOAuthClientId = '226205689391-7cvoq5ionp176vnmbsiphempn1rt0kp4.apps.googleusercontent.com';
