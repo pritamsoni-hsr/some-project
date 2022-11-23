d8754d64c10ec28a2ff445a66a9ffc76911e7976
Pritam Soni <23050213+pritamsoni-hsr@users.noreply.github.com>
2022-11-24 04:22:43 +0530

    add card icon

---
+++ app/src/pages/Finance/components/CardIcon.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+
+import { Pressable, Text } from '@app/components';
+import { useTheme } from '@app/hooks';
+
+type Props = {
+  id?: string;
+  icon: string;
+  name: string;
+  width?: number;
+  selected: boolean;
+  onPress: (e: string) => void;
+};
+
+export const CardIcon = (props: Props) => {
+  const { theme } = useTheme();
+  const backgroundColor = theme[props.selected ? 'background-basic-color-4' : 'background-basic-color-1'];
+
+  const handlePress = () => props.onPress(props.id);
+  return (
+    <Pressable key={props.name} style={[styles.card, { backgroundColor }]} onPress={handlePress}>
+      <Text size={40} style={styles.icon}>
+        {props.icon}
+      </Text>
+      <Text center ellipsizeMode={'tail'} numberOfLines={1} style={styles.label} variant={'footnote'}>
+        {props.name}
+      </Text>
+    </Pressable>
+  );
+};
+
+const styles = StyleSheet.create({
+  card: {
+    aspectRatio: 1,
+    borderRadius: 12,
+  },
+  icon: {
+    marginVertical: 5,
+    alignSelf: 'center',
+  },
+  label: {
+    textTransform: 'capitalize',
+  },
+});
